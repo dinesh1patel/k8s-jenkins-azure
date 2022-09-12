53d31cc098675a426479c76fdc7697e829ba3bc5
dinesh1patel <74076102+dinesh1patel@users.noreply.github.com>
2022-09-12 16:09:48 +0100

    Update JhooqDockerDemoController.java

--- src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -8,6 +8,6 @@ public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Jenkins Pipeline Test 6";
+        return "Jenkins Pipeline Test 7";
     }
 }
