8f10e5b4cc5133a358862c57188da483a630602e
Dinesh Patel <dinesh.patel@hmcts.net>
2022-09-26 12:14:17 +0100

    Updates 3

--- src/main/java/com/jhooq/Jhooqk8s/controller/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/controller/JhooqDockerDemoController.java
@@ -1,9 +1,11 @@
-package com.jhooq.Jhooqk8s.ws;
+package com.jhooq.Jhooqk8s.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@RequestMapping("api/v1")
 public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
